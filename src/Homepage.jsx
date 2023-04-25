5a4fefea4b0f0f75bc06438b51088e5c32983663
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-25 14:59:37 +0300

    Initial commit

---
+++ src/Homepage.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import data from "./data/data";
+
+const Homepage = () => {
+  const [duration, setDuration] = useState("daily");
+  return (
+    <div className="homepage">
+      <div className="container">
+        <div className="left-card">
+          <div className="card-person">
+            <img className="jeremy" src="/image-jeremy.png" alt="" />
+            <p className="report">Report by</p>
+            <p className="name">Jeremy Robson</p>
+          </div>
+          <div className="timeline">
+            <p onClick={(e) => setDuration(e.target.textContent.toLowerCase())}>
+              Daily
+            </p>
+            <p onClick={(e) => setDuration(e.target.textContent.toLowerCase())}>
+              Weekly
+            </p>
+            <p onClick={(e) => setDuration(e.target.textContent.toLowerCase())}>
+              Monthly
+            </p>
+          </div>
+        </div>
+        <div className="right-cards">
+          {data.map((activity) => {
+            return (
+              <div
+                style={{ background: `url(${activity.background}) no-repeat left top, linear-gradient(${activity.gradient}, #d13531)` }}
+                key={activity.title}
+                className="activity-card"
+              >
+                <div className="activity">
+                  <p>{activity.title}</p>
+                  <p className="hours">
+                    {duration === "daily"
+                      ? activity.timeframes.daily.current
+                      : duration === "weekly"
+                      ? activity.timeframes.weekly.current
+                      : activity.timeframes.monthly.current}
+                    hrs
+                  </p>
+                  <p className="last-week">                
+                  Last week - {duration === "daily"
+                      ? activity.timeframes.daily.previous
+                      : duration === "weekly"
+                      ? activity.timeframes.weekly.previous
+                      : activity.timeframes.monthly.previous}
+                    hrs
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/data/data.js
@@ -0,0 +1,118 @@
+const data = [
+  {
+    gradient: "yellow",
+    background: "/icon-work.svg",
+    title: "Work",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
+      },
+      weekly: {
+        current: 32,
+        previous: 36,
+      },
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
+  },
+  {
+    gradient: "blue",
+    background: "/icon-play.svg",
+    title: "Play",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
+      },
+      weekly: {
+        current: 10,
+        previous: 8,
+      },
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
+  },
+  {
+    gradient: "maroon",
+    background: "/icon-study.svg",
+    title: "Study",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 7,
+      },
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
+  },
+  {
+    gradient: "lightgreen",
+    background: "/icon-exercise.svg",
+    title: "Exercise",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 5,
+      },
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
+  },
+  {
+    gradient: "teal",
+    background: "/icon-social.svg",
+    title: "Social",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
+      },
+      weekly: {
+        current: 5,
+        previous: 10,
+      },
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
+  },
+  {
+    gradient: "pink",
+    background: "/icon-self-care.svg",
+    title: "Self Care",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 2,
+        previous: 2,
+      },
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
+];
+
+export default data;
